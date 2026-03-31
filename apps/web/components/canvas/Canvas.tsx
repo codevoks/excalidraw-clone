4cce96379e1d5ae45e427158851f92fd904778ff
codevoks <vanshkumar841@gmail.com>
2026-03-31 21:14:26 +0530

    Enhance Canvas component with click event handling and set dimensions
    - Implemented a click event handler in the Canvas component to draw rectangles at the clicked position.
    - Set explicit height and width attributes for the canvas element to ensure proper rendering.

--- apps/web/components/canvas/Canvas.tsx
+++ apps/web/components/canvas/Canvas.tsx
@@ -1,14 +1,40 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 
 export function Canvas() {
   const containerRef = useRef<HTMLDivElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
+  //   useEffect(() => {
+  //     if (canvasRef.current) {
+  //       const ctx = canvasRef.current.getContext("2d");
+  //       ctx?.strokeRect(200, 200, 40, 50);
+  //     }
+  //   }, []);
+
+  const handleClick = (event: MouseEvent) => {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const ctx = canvas.getContext("2d");
+      const rect = canvas.getBoundingClientRect();
+      const scaleX = canvas.width / rect.width;
+      const scaleY = canvas.height / rect.height;
+      const canvasX = (event.clientX - rect.left) * scaleX;
+      const canvasY = (event.clientY - rect.top) * scaleY;
+      ctx?.strokeRect(canvasX, canvasY, 50, 50);
+    }
+  };
+
   return (
     <div ref={containerRef} className="min-h-0 w-full flex-1">
-      <canvas ref={canvasRef} className="h-full w-full bg-slate-800" />
+      <canvas
+        ref={canvasRef}
+        className="h-full w-full bg-slate-800"
+        height={600}
+        width={400}
+        onClick={handleClick}
+      />
     </div>
   );
 }
