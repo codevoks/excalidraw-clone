abe4335af4e3b8c30120a13e78ef565cd1a29534
codevoks <vanshkumar841@gmail.com>
2026-04-03 19:38:44 +0530

    Add error logging in WebSocket connection handling
    - Enhanced error handling in the WebSocket server by adding console error logging for exceptions during message processing, improving debugging and stability.

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -25,7 +25,9 @@ wss.on("connection", function connection(ws) {
           client.send(incomingMessage);
         }
       });
-    } catch (error) {}
+    } catch (error) {
+      console.error(error);
+    }
   });
 
   ws.send("something");
