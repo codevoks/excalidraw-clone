d569a2a2d42a0ac27014156a48e91bf20375a31c
codevoks <vanshkumar841@gmail.com>
2026-04-05 17:25:07 +0530

    Add error handling to pubsub functions
    - Wrapped the `publish`, `subscribe`, and `unsubscribe` functions in try-catch blocks to handle potential errors during message operations.
    - Added console logging for error messages to improve debugging and monitoring of the pubsub module's functionality.

--- packages/pubsub/src/pubsub.ts
+++ packages/pubsub/src/pubsub.ts
@@ -1,16 +1,28 @@
 import { publisher, subscriber } from "./client";
 
 export async function publish(channel: string, message: string): Promise<void> {
-  await publisher.publish(channel, message);
+  try {
+    await publisher.publish(channel, message);
+  } catch (error) {
+    console.log("Error publishing " + error);
+  }
 }
 
 export async function subscribe(
   channel: string,
   onMessage: (message: string, channel: string) => void,
 ): Promise<void> {
-  await subscriber.subscribe(channel, onMessage);
+  try {
+    await subscriber.subscribe(channel, onMessage);
+  } catch (error) {
+    console.log("Error subscribing " + error);
+  }
 }
 
 export async function unsubscribe(channel: string): Promise<void> {
-  await subscriber.unsubscribe(channel);
+  try {
+    await subscriber.unsubscribe(channel);
+  } catch (error) {
+    console.log("Error unsubscribing " + error);
+  }
 }
